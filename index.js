b5b717ac1f8f4f02853c5d0379b1f3e0d54967bb
Enoch2k2 <enoch2k2@hotmail.com>
2020-04-17 13:06:48 -0500

    added sockets

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const socket = require("socket.io");
 const app = express();
 
 
@@ -6,12 +7,40 @@ app.use(express.static('public'));
 
 app.get('/', (req, resp) => resp.sendFile('index.html'));
 
-app.listen(process.env.PORT || 3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log('listening on port: 3000')
 })
 
+const io = socket(server);
+
+const users = [];
 
 // const io = socket(server); adds a socket.io listener to our server
+
+io.on('connection', function (socket) {
+  console.log('made socket connection', socket.id);
+
+  socket.on('typing', function (data) {
+    socket.broadcast.emit('typing', data);
+  })
+
+  socket.on('message', function (data) {
+    io.sockets.emit('message', data);
+  })
+
+  socket.on('login', function (data) {
+    let user = { ...data, id: socket.id };
+    users.push(user);
+    io.sockets.emit('login', { user: data, users: users });
+  })
+
+  socket.on('disconnect', function () {
+    let user = users.find(user => user.id === socket.id);
+    let index = users.indexOf(user);
+    users.splice(index, 1);
+    io.sockets.emit('logout', { user, users });
+  })
+})
 
 // responds to clients connecting to our server
 // io.on('connection', function (socket) {
--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,6 @@
+// make a socket connection to our server
+const socket = io.connect('http://localhost:3000');
+
 // On Page Load //
 document.addEventListener('DOMContentLoaded', function () {
   toggleChat();
@@ -9,6 +12,7 @@ let showChat = true;
 // DOM Getters //
 const getUsernameForm = () => document.querySelector('#form');
 const getUsername = () => document.querySelector('#username');
+const getUsers = () => document.getElementById('users');
 const getMessage = () => document.querySelector('#message');
 const getTyper = () => document.querySelector('#typer');
 const getChatWindow = () => document.getElementById('Chat-Window');
@@ -28,36 +32,75 @@ function toggleChat() {
   }
 }
 
+function refreshUsers(users) {
+  getUsers().innerHTML = '';
+  users.forEach(user => {
+    let li = document.createElement('li');
+    li.innerText = user.username;
+    getUsers().appendChild(li);
+  })
+}
+
 // Event Listeners //
 getUsernameForm().addEventListener('submit', e => {
   e.preventDefault();
   toggleChat();
   getUsername().innerHTML = document.getElementById('handle').value;
+  socket.emit('login', { username: getUsername().innerText });
   getUsernameForm().style.display = 'none';
 })
 
 getMessage().addEventListener('keydown', function () {
-  getTyper().innerHTML = '';
-  let p = document.createElement('p');
-  let em = document.createElement('em');
-  em.innerText = getUsername().innerText + " is typing a message...";
-  p.appendChild(em);
-  getTyper().appendChild(p);
-  getChatWindow().scrollTop = getChatWindow().scrollHeight;
+  socket.emit('typing', { username: getUsername().innerText });
 })
 
 getMessageForm().addEventListener('submit', function (e) {
   e.preventDefault();
   // submit message
-  getTyper().innerHTML = ""
 
+  socket.emit('message', { username: getUsername().innerText, message: getMessage().value })
+
+  getMessage().value = ""
+})
+
+socket.on('login', function (data) {
   let p = document.createElement('p');
   let strong = document.createElement('strong');
-  strong.innerText = getUsername().innerText;
+  strong.innerText = `${data.user.username} has logged in`;
   p.appendChild(strong);
-  p.insertAdjacentText("beforeend", ": " + getMessage().value);
+  getOutput().appendChild(p);
+  getChatWindow().scrollTop = getChatWindow().scrollHeight;
+  refreshUsers(data.users);
+})
+
+socket.on('message', function (data) {
+  getTyper().innerHTML = ""
+  let p = document.createElement('p');
+  let strong = document.createElement('strong');
+  strong.innerText = data.username;
+  p.appendChild(strong);
+  p.insertAdjacentText("beforeend", ": " + data.message);
   getOutput().appendChild(p);
   getChatWindow().scrollTop = getChatWindow().scrollHeight;
   // erase message window
-  getMessage().value = ""
+})
+
+socket.on('typing', function (data) {
+  getTyper().innerHTML = '';
+  let p = document.createElement('p');
+  let em = document.createElement('em');
+  em.innerText = data.username + " is typing a message...";
+  p.appendChild(em);
+  getTyper().appendChild(p);
+  getChatWindow().scrollTop = getChatWindow().scrollHeight;
+})
+
+socket.on('logout', function (data) {
+  let p = document.createElement('p');
+  let strong = document.createElement('strong');
+  strong.innerText = `${data.user.username} has logged off`;
+  p.appendChild(strong);
+  getOutput().appendChild(p);
+  getChatWindow().scrollTop = getChatWindow().scrollHeight;
+  refreshUsers(data.users);
 })
